521f3e0f77d3f5c63acfdda5afa553d7d853feff
Jordan Tymburski <hello@jordantymburski.com>
2021-03-05 17:52:41 -0800

    Fix to output of mac script, incorrectly self-identifying as windows

--- resource/executable/fetch-doom-mac.sh
+++ resource/executable/fetch-doom-mac.sh
@@ -7,7 +7,7 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-echo "## WINDOWS DOWNLOADER ##"
+echo "## OS X DOWNLOADER ##"
 
 source "$SCRIPT_DIR/fetch-doom-base"
 
