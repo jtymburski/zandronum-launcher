f558d490c09e67590c924f72c772d73329bfd379
Jordan Tymburski <hello@jordantymburski.com>
2021-09-06 16:46:37 -0700

    Handle multiple local addresses by prioritizing based on the subnet

--- include/network/networkinfo.h
+++ include/network/networkinfo.h
@@ -2,10 +2,15 @@
 #define NETWORKINFO_H
 
 #include <QNetworkInterface>
+#include <QRegularExpression>
 #include <QString>
 
 class NetworkInfo
 {
+private:
+  /* Local network address ordered filters to apply when deciding on the address to return  */
+  const static QStringList LOCAL_ADDRESS_FILTERS;
+
 public:
   /* Returns if the address is a valid IPv4 format */
   bool isValidAddress(QString address);
--- src/network/networkinfo.cpp
+++ src/network/networkinfo.cpp
@@ -1,5 +1,11 @@
 #include "network/networkinfo.h"
 
+/* Local network address ordered filters to apply when deciding on the address to return  */
+const QStringList NetworkInfo::LOCAL_ADDRESS_FILTERS = {
+  "^192\\.",
+  "^10\\."
+};
+
 /**
  * Returns if the address provided is valid and can be parsed. If this returns true,
  * it is expected that this should be compatible with any network interface.
@@ -17,6 +23,9 @@ bool NetworkInfo::isValidAddress(QString address)
  */
 QString NetworkInfo::localAddress()
 {
+  QStringList candidate_addresses;
+
+  // Filter through all interfaces to reduce the number of candidates
   QList<QNetworkInterface> interfaces = QNetworkInterface::allInterfaces();
   for(auto interface = interfaces.begin(); interface != interfaces.end(); interface++)
   {
@@ -35,11 +44,23 @@ QString NetworkInfo::localAddress()
           // Only IPv4 based addresses are supported for server hosting
           QHostAddress ip_address = address->ip();
           if(!ip_address.isNull() && ip_address.protocol() == QAbstractSocket::IPv4Protocol)
-            return ip_address.toString();
+            candidate_addresses.append(ip_address.toString());
         }
       }
     }
   }
 
-  return QString();
+  // Sort the list to ensure consistency
+  candidate_addresses.sort();
+
+  // Prioritize the filters for finding the preferred address
+  for(auto const &filter : LOCAL_ADDRESS_FILTERS)
+  {
+    QStringList filtered_addresses = candidate_addresses.filter(QRegularExpression(filter));
+    if(!filtered_addresses.isEmpty())
+      return filtered_addresses.first();
+  }
+
+  // Otherwise, just return the first in the list
+  return candidate_addresses.isEmpty() ? QString() : candidate_addresses.first();
 }
